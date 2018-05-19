84facf75734c4575c538a76263c777cf5889f6ae
Justin Carter <justin@starkandwayne.com>
2018-05-19 14:43:15 +0200

    debug deploy-staging

--- ci/tasks/deploy-staging.sh
+++ ci/tasks/deploy-staging.sh
@@ -11,4 +11,5 @@ gcloud auth activate-service-account --key-file ./account.json
 
 mkdir out
 tar -zxvf misthos-build/misthos-build-*.tgz -C out
+ls out
 gsutil -m cp -r out/build/* gs://testnet.misthos.io
