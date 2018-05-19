be2974748a0a026dcb3127bca13735e104b87624
Justin Carter <justin@starkandwayne.com>
2018-05-19 14:45:28 +0200

    Fix deploy path

--- ci/tasks/deploy-staging.sh
+++ ci/tasks/deploy-staging.sh
@@ -9,7 +9,5 @@ EOF
 
 gcloud auth activate-service-account --key-file ./account.json
 
-mkdir out
-tar -zxvf misthos-build/misthos-build-*.tgz -C out
-ls out
-gsutil -m cp -r out/build/* gs://testnet.misthos.io
+tar -zxvf misthos-build/misthos-build-*.tgz
+gsutil -m cp -r repo/build/* gs://testnet.misthos.io
