decd581291f9dce247ea8d27aff9814dd821663f
Justin Carter <justin@starkandwayne.com>
2018-05-28 09:14:58 +0200

    Fix build.sh again

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf dist/*
-NODE_ENV=production node_modules/.bin/webpack --mode production
-for file in "$(ls public | grep -v 'index.html')" ; do
-  cp "public/$file" dist/;
+# rm -rf dist/*
+# NODE_ENV=production node_modules/.bin/webpack --mode production
+for file in $(ls public | grep -v 'index.html') ; do
+  cp "public/${file}" dist/;
 done
