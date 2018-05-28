816c700bb4e435546d5c258c1335ee722d5980d5
Justin Carter <justin@starkandwayne.com>
2018-05-28 08:52:58 +0200

    Fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 rm -rf dist/*
 NODE_ENV=production node_modules/.bin/webpack --mode production
 for file in "$(ls public | grep -v 'index.html')" ; do
-  cp "public/$file dist/";
+  cp "public/$file" dist/;
 done
