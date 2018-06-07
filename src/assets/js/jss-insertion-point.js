ef4bbdb488c5467865d91fe0349a577ea89319df
Justin Carter <justin@starkandwayne.com>
2018-06-07 16:19:09 +0200

    Don't access document obj server side

--- src/assets/js/jss-insertion-point.js
+++ src/assets/js/jss-insertion-point.js
@@ -2,7 +2,9 @@ import { create } from 'jss';
 import { jssPreset } from 'material-ui/styles';
 
 export default function() {
-    let jss = create(jssPreset());
+  let jss = create(jssPreset());
+  if (typeof document !== "undefined") {
     jss.options.insertionPoint = document.getElementById('jss-insertion-point');
-    return jss;
+  };
+  return jss;
 }
