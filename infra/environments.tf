bbf3596ef598f44f89737b00b7765e2d60ac4df2
Justin Carter <justin@starkandwayne.com>
2018-06-08 10:45:20 +0200

    Add misthos-web-staging bucket

--- infra/environments.tf
+++ infra/environments.tf
@@ -4,6 +4,29 @@ provider "google" {
   region      = "europe-west1"
 }
 
+resource "google_storage_bucket" "misthos-web-staging" {
+  name          = "web-staging.misthos.io"
+  location      = "EU"
+  storage_class = "MULTI_REGIONAL"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page = "index.html"
+  }
+}
+
+resource "google_storage_bucket_iam_member" "web-staging-concourse" {
+  bucket = "${google_storage_bucket.misthos-web-staging.name}"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:concourse@misthos-173012.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "web-staging-public" {
+  bucket = "${google_storage_bucket.misthos-web-staging.name}"
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
 resource "google_storage_bucket" "misthos-app-staging" {
   name          = "staging.misthos.io"
   location      = "EU"
