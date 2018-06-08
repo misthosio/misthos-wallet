13364c582650271e8300f742523e55866d850015
Justin Carter <justin@starkandwayne.com>
2018-06-08 10:48:37 +0200

    Add deploy to web-staging

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -6,8 +6,13 @@ cat <<EOF > account.json
 ${GCP_SERVICE_ACCOUNT}
 EOF
 
+WEB_PATH="";
+if [[ "${WEB}" == "true" ]]; then
+  WEB_PATH="web-";
+fi
+
 
 gcloud auth activate-service-account --key-file ./account.json
 
-tar -zxvf misthos-build/misthos-build-*.tgz
-gsutil -m cp -r repo/dist/* "gs://${DEPLOY_ENVIRONMENT}.misthos.io"
+tar -zxvf misthos-${WEB_PATH}build/misthos-${WEB_PATH}build-*.tgz
+gsutil -m cp -r repo/${WEB_PATH}dist/* "gs://${WEB_PATH}${DEPLOY_ENVIRONMENT}.misthos.io"
