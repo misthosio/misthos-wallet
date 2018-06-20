8a776af1a8b43d88d5612adc58ecb845a8321e56
Justin Carter <justin@starkandwayne.com>
2018-06-20 08:27:03 +0200

    Fix path to favicon on website

--- pages/_document.js
+++ pages/_document.js
@@ -26,21 +26,21 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="static/apple-touch-icon.png"
+            href="/static/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="static/favicon-32x32.png"
+            href="/static/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="static/favicon-16x16.png"
+            href="/static/favicon-16x16.png"
           />
-          <link rel="manifest" href="static/manifest.json" />
+          <link rel="manifest" href="/static/manifest.json" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="theme-color" content="#ffffff" />
           <link
