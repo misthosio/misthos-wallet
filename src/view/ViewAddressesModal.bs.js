899acd5f38fd9b7b216306da957d74a1f8a29fea
Ruben Koster <rkoster@starkandwayne.com>
2018-07-03 22:24:35 +0200

    Apply AddressList styling to address details modal

--- src/view/ViewAddressesModal.bs.js
+++ src/view/ViewAddressesModal.bs.js
@@ -2,13 +2,24 @@
 'use strict';
 
 var BTC = require("../application/wallet/BTC.bs.js");
+var Css = require("bs-css/src/Css.js");
+var Grid = require("./components/Grid.bs.js");
 var Curry = require("bs-platform/lib/js/curry.js");
+var Icons = require("./Icons.bs.js");
+var Theme = require("./Theme.bs.js");
 var React = require("react");
+var Partner = require("./components/Partner.bs.js");
 var Belt_Set = require("bs-platform/lib/js/belt_Set.js");
 var Belt_List = require("bs-platform/lib/js/belt_List.js");
+var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
+var ScrollList = require("./components/ScrollList.bs.js");
 var ViewCommon = require("./ViewCommon.bs.js");
+var MTypography = require("./components/MTypography.bs.js");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
 var PrimitiveTypes = require("../application/PrimitiveTypes.bs.js");
+var MaterialUi_List = require("@jsiebern/bs-material-ui/src/MaterialUi_List.bs.js");
+var MaterialUi_Collapse = require("@jsiebern/bs-material-ui/src/MaterialUi_Collapse.bs.js");
+var MaterialUi_IconButton = require("@jsiebern/bs-material-ui/src/MaterialUi_IconButton.bs.js");
 
 function statusToString(param) {
   switch (param) {
@@ -36,18 +47,71 @@ function addressTypeToString(param) {
 
 var component = ReasonReact.statelessComponent("AddressesModal");
 
+var grid = Css.style(/* :: */[
+      Css.display(Css.grid),
+      /* :: */[
+        Css.unsafe("gridTemplateColumns", "[begin] 5fr 1fr min-content [end]"),
+        /* [] */0
+      ]
+    ]);
+
+var header = Css.style(/* :: */[
+      Css.borderBottom(Css.px(1), /* solid */12956715, Css.hex("979797")),
+      /* :: */[
+        Css.padding2(Css.px(Theme.space(2)), Css.px(Theme.space(3))),
+        /* [] */0
+      ]
+    ]);
+
+var summary = Css.style(/* :: */[
+      Css.padding2(Css.px(Theme.space(2)), Css.px(Theme.space(3))),
+      /* [] */0
+    ]);
+
+var details = Css.style(/* :: */[
+      Css.unsafe("gridColumn", "begin / end"),
+      /* :: */[
+        Css.borderBottom(Css.px(1), /* solid */12956715, Css.hex("979797")),
+        /* :: */[
+          Css.paddingBottom(Css.px(Theme.space(5))),
+          /* [] */0
+        ]
+      ]
+    ]);
+
+var detailsGrid = Css.style(/* :: */[
+      Css.display(Css.grid),
+      /* :: */[
+        Css.gridGap(Css.px(Theme.space(3))),
+        /* :: */[
+          Css.unsafe("gridTemplateColumns", "[begin] 1fr 1fr [end]"),
+          /* :: */[
+            Css.padding2(Css.px(0), Css.px(Theme.space(3))),
+            /* [] */0
+          ]
+        ]
+      ]
+    ]);
+
+var Styles = /* module */[
+  /* grid */grid,
+  /* header */header,
+  /* summary */summary,
+  /* details */details,
+  /* detailsGrid */detailsGrid
+];
+
 function make(viewData, _) {
   var renderExpandedInfo = function (info) {
-    var __x = String(info[/* nCoSigners */1]) + ("-" + (String(info[/* nCustodians */2]) + "; ["));
-    var __x$1 = Belt_Set.reduceU(info[/* custodians */0], __x, (function (res, c) {
-            var match = Curry._1(info[/* isPartner */7], c);
-            return res + (PrimitiveTypes.UserId[/* toString */0](c) + ((
-                        match ? "" : " - Ex-Partner"
-                      ) + ", "));
-          })) + "] [";
-    return React.createElement("div", undefined, ViewCommon.text(Belt_List.reduceU(Belt_List.concat(info[/* currentUtxos */5], info[/* spentInputs */6]), __x$1, (function (res, input) {
-                          return res + (BTC.format(input[/* value */3]) + ", ");
-                        })) + ("] " + addressTypeToString(info[/* addressType */3]))));
+    return ReasonReact.element(/* None */0, /* None */0, MaterialUi_Collapse.make(/* Some */[details], /* None */0, /* None */0, /* Some */[true], /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* array */[React.createElement("div", {
+                          className: detailsGrid
+                        }, React.createElement("div", undefined, ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Title */594052472, /* None */0, /* Some */[true], /* None */0, /* None */0, /* array */[ViewCommon.text("Custodians")])), ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Body1 */-904051921, /* None */0, /* Some */[true], /* None */0, /* None */0, /* array */[ViewCommon.text("This is a " + (String(info[/* nCoSigners */1]) + ("-of-" + (String(info[/* nCustodians */2]) + " address with the following custodians:"))))])), ReasonReact.element(/* None */0, /* None */0, MaterialUi_List.make(/* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* array */[Belt_Array.map(Belt_Set.toArray(info[/* custodians */0]), (function (partnerId) {
+                                              var match = Curry._1(info[/* isPartner */7], partnerId);
+                                              var status = match ? /* None */0 : /* Some */[ViewCommon.text(" - Ex-Partner")];
+                                              return ReasonReact.element(/* None */0, /* None */0, Partner.make(partnerId, /* None */0, /* None */0, status, /* None */0, /* array */[]));
+                                            }))]))), React.createElement("div", undefined, ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Title */594052472, /* None */0, /* Some */[true], /* None */0, /* None */0, /* array */[ViewCommon.text("OVERVIEW")])), ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Body1 */-904051921, /* None */0, /* Some */[true], /* None */0, /* None */0, /* array */[ViewCommon.text("ADDRESS BALANCE: TODO")])), Belt_Array.map(Belt_List.toArray(Belt_List.concat(info[/* currentUtxos */5], info[/* spentInputs */6])), (function (input) {
+                                    return ViewCommon.text(BTC.format(input[/* value */3]));
+                                  }))))]));
   };
   return /* record */[
           /* debugName */component[/* debugName */0],
@@ -63,12 +127,23 @@ function make(viewData, _) {
               var infos = Belt_List.toArray(Belt_List.keepMapU(viewData[/* infos */0], (function (info) {
                           if (info[/* addressType */0] !== /* Change */0 || info[/* balance */5].gt(BTC.zero)) {
                             var expandedInfo = Curry._1(viewData[/* addressDetails */1], info);
-                            return /* Some */[React.createElement("li", undefined, React.createElement("div", undefined, ViewCommon.text(info[/* address */2] + (" " + (BTC.format(info[/* balance */5]) + (" " + statusToString(info[/* addressStatus */4])))))), renderExpandedInfo(expandedInfo))];
+                            return /* Some */[/* array */[
+                                      ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Body2 */-904051920, /* Some */[summary], /* None */0, /* None */0, /* None */0, /* array */[ViewCommon.text(info[/* address */2])])),
+                                      ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Body2 */-904051920, /* Some */[summary], /* None */0, /* None */0, /* None */0, /* array */[ViewCommon.text(statusToString(info[/* addressStatus */4]))])),
+                                      ReasonReact.element(/* None */0, /* None */0, MaterialUi_IconButton.make(/* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* array */[Icons.chevronDown])),
+                                      renderExpandedInfo(expandedInfo)
+                                    ]];
                           } else {
                             return /* None */0;
                           }
                         })));
-              return React.createElement("div", undefined, React.createElement("ul", undefined, infos));
+              return ReasonReact.element(/* None */0, /* None */0, Grid.make(/* Some */[ViewCommon.text("Wallet Address History")], /* None */0, /* None */0, /* None */0, /* Some */[React.createElement("div", {
+                                    className: ScrollList.containerStyles
+                                  }, ReasonReact.element(/* None */0, /* None */0, ScrollList.make(/* array */[React.createElement("div", {
+                                                  className: grid
+                                                }, ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Body2 */-904051920, /* Some */[header], /* None */0, /* None */0, /* None */0, /* array */[ViewCommon.text("WALLET ADDRESS")])), ReasonReact.element(/* None */0, /* None */0, MTypography.make(/* Body2 */-904051920, /* Some */[header], /* None */0, /* None */0, /* None */0, /* array */[ViewCommon.text("STATUS")])), React.createElement("span", {
+                                                      className: header
+                                                    }), infos)])))], /* None */0, /* None */0, /* None */0, /* array */[]));
             }),
           /* initialState */component[/* initialState */10],
           /* retainedProps */component[/* retainedProps */11],
@@ -90,5 +165,6 @@ exports.ViewData = ViewData;
 exports.statusToString = statusToString;
 exports.addressTypeToString = addressTypeToString;
 exports.component = component;
+exports.Styles = Styles;
 exports.make = make;
 /* component Not a pure module */
--- src/view/ViewAddressesModal.re
+++ src/view/ViewAddressesModal.re
@@ -19,36 +19,89 @@ let addressTypeToString =
 
 let component = ReasonReact.statelessComponent("AddressesModal");
 
+module Styles = {
+  open Css;
+  let grid =
+    style([
+      display(grid),
+      unsafe("gridTemplateColumns", "[begin] 5fr 1fr min-content [end]"),
+    ]);
+  let header =
+    style([
+      borderBottom(px(1), `solid, hex("979797")),
+      padding2(~v=px(Theme.space(2)), ~h=px(Theme.space(3))),
+    ]);
+  let summary =
+    style([padding2(~v=px(Theme.space(2)), ~h=px(Theme.space(3)))]);
+  let details =
+    style([
+      unsafe("gridColumn", "begin / end"),
+      borderBottom(px(1), `solid, hex("979797")),
+      paddingBottom(px(Theme.space(5))),
+    ]);
+  let detailsGrid =
+    style([
+      display(Css.grid),
+      gridGap(px(Theme.space(3))),
+      unsafe("gridTemplateColumns", "[begin] 1fr 1fr [end]"),
+      padding2(~v=px(0), ~h=px(Theme.space(3))),
+    ]);
+};
+
 let make = (~viewData: ViewData.t, _children) => {
   let renderExpandedInfo = (info: ViewData.addressDetails) =>
-    <div>
-      (
-        (
-          (
-            string_of_int(info.nCoSigners)
-            ++ "-"
-            ++ string_of_int(info.nCustodians)
-            ++ "; ["
-            |. Set.reduceU(info.custodians, _, (. res, c) =>
-                 res
-                 ++ UserId.toString(c)
-                 ++ (c |> info.isPartner ? "" : " - Ex-Partner")
-                 ++ ", "
-               )
-          )
-          ++ "] ["
-          |. List.reduceU(
-               List.concat(info.currentUtxos, info.spentInputs),
-               _,
-               (. res, input: Network.txInput) =>
-               res ++ (input.value |> BTC.format) ++ ", "
-             )
-        )
-        ++ "] "
-        ++ addressTypeToString(info.addressType)
-        |> text
-      )
-    </div>;
+    MaterialUi.(
+      <Collapse className=Styles.details in_=true>
+        <div className=Styles.detailsGrid>
+          <div>
+            <MTypography gutterBottom=true variant=`Title>
+              ("Custodians" |> text)
+            </MTypography>
+            <MTypography gutterBottom=true variant=`Body1>
+              (
+                "This is a "
+                ++ string_of_int(info.nCoSigners)
+                ++ "-of-"
+                ++ string_of_int(info.nCustodians)
+                ++ " address with the following custodians:"
+                |> text
+              )
+            </MTypography>
+            <List>
+              (
+                Array.map(
+                  info.custodians |> Set.toArray,
+                  (partnerId: UserId.t) => {
+                    let status =
+                      partnerId |> info.isPartner ?
+                        None : Some(" - Ex-Partner" |> text);
+                    <Partner partnerId ?status />;
+                  },
+                )
+                |> ReasonReact.array
+              )
+            </List>
+          </div>
+          <div>
+            <MTypography gutterBottom=true variant=`Title>
+              ("OVERVIEW" |> text)
+            </MTypography>
+            <MTypography gutterBottom=true variant=`Body1>
+              ("ADDRESS BALANCE: TODO" |> text)
+            </MTypography>
+            (
+              Array.map(
+                Belt.List.concat(info.currentUtxos, info.spentInputs)
+                |> Belt.List.toArray,
+                (input: Network.txInput) =>
+                input.value |> BTC.format |> text
+              )
+              |> ReasonReact.array
+            )
+          </div>
+        </div>
+      </Collapse>
+    );
   {
     ...component,
     render: _ => {
@@ -59,19 +112,19 @@ let make = (~viewData: ViewData.t, _children) => {
                  || info.balance
                  |> BTC.gt(BTC.zero)) {
                let expandedInfo = viewData.addressDetails(info);
-               <li>
-                 <div>
-                   (
-                     info.address
-                     ++ " "
-                     ++ (info.balance |> BTC.format)
-                     ++ " "
-                     ++ statusToString(info.addressStatus)
-                     |> text
-                   )
-                 </div>
-                 (expandedInfo |> renderExpandedInfo)
-               </li>
+               [|
+                 <MTypography className=Styles.summary variant=`Body2>
+                   (info.address |> text)
+                 </MTypography>,
+                 <MTypography className=Styles.summary variant=`Body2>
+                   (statusToString(info.addressStatus) |> text)
+                 </MTypography>,
+                 <MaterialUi.IconButton>
+                   Icons.chevronDown
+                 </MaterialUi.IconButton>,
+                 expandedInfo |> renderExpandedInfo,
+               |]
+               |> ReasonReact.array
                |. Some;
              } else {
                None;
@@ -79,7 +132,25 @@ let make = (~viewData: ViewData.t, _children) => {
            )
         |> List.toArray
         |> ReasonReact.array;
-      <div> <ul> infos </ul> </div>;
+      <Grid
+        title1=("Wallet Address History" |> text)
+        area3={
+          <div className=ScrollList.containerStyles>
+            <ScrollList>
+              <div className=Styles.grid>
+                <MTypography className=Styles.header variant=`Body2>
+                  ("WALLET ADDRESS" |> text)
+                </MTypography>
+                <MTypography className=Styles.header variant=`Body2>
+                  ("STATUS" |> text)
+                </MTypography>
+                <span className=Styles.header />
+                infos
+              </div>
+            </ScrollList>
+          </div>
+        }
+      />;
     },
   };
 };
