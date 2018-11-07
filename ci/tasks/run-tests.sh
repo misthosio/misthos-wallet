7bba4745a937ddd1bca6fbef3ba3a8ae307fc7c4
Ruben Koster <rkoster@starkandwayne.com>
2018-11-07 23:17:59 +0100

    Update yarn and regen yarn.lock with sha512 hashes

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -5,7 +5,6 @@ set -e
 tar -zxvf bundled-deps/bundled-deps-*.tgz > /dev/null
 
 if [[ $(diff -q deps/yarn.lock repo/yarn.lock) ]]; then
-  diff -q deps/yarn.lock repo/yarn.lock
   echo "Deps are not up to date!"
   exit 1
 fi
