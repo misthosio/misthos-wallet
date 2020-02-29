7b85e81321f8372967a77fb9b73bdd12aaed50bb
Justin Carter <justin@misthos.io>
2020-02-29 05:40:12 +0100

    Fix scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -rf dist/*
+rm -rf dist
+mkdir dist
+
 NODE_ENV=production node_modules/.bin/webpack --mode production
 for file in $(ls static | grep -v 'index.html') ; do
   cp "static/${file}" dist/;
