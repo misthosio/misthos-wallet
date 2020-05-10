764cb1acba3b992ffe2df86ce00a16f5ab49ede7
Justin Carter <justin@misthos.io>
2020-05-11 00:35:49 +0200

    Add cp -r to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ mkdir dist
 
 NODE_ENV=production node_modules/.bin/webpack --mode production
 for file in $(ls static | grep -v 'index.html') ; do
-  cp "static/${file}" dist/;
+  cp -r "static/${file}" dist/;
 done
